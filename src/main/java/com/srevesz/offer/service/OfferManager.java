2b2b2cf5bd2fa23b13501142357b690021761388
Szabolcs Revesz <szrevesz@gmail.com>
2017-11-04 04:58:37 +0000

    03 - Reverted the business logic in OfferManager

--- src/main/java/com/srevesz/offer/service/OfferManager.java
+++ src/main/java/com/srevesz/offer/service/OfferManager.java
@@ -8,7 +8,6 @@ import com.srevesz.offer.model.Offer;
 public class OfferManager {
 
 	public Offer save(String goods, String description, Double price, String currency) {
-		Offer offer = new Offer(goods, description, price, currency);
-		return offer;
+		return null;
 	}
 }
